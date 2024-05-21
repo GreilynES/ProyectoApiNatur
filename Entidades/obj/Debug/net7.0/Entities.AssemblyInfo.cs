af151b5a892e38745cbefd4a3b50a1a24a58646e
KaMendez04 <katherynmq@gmail.com>
2024-05-21 16:44:47 -0600

    carpetas

--- Entidades/obj/Debug/net7.0/Entities.AssemblyInfo.cs
+++ Entidades/obj/Debug/net7.0/Entities.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
+[assembly: System.Reflection.AssemblyProductAttribute("Entities")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
