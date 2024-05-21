83b5910fd46f2248b99fd56597fcb324f8c6b2a1
GreilynES <greilynesquivel@gmail.com>
2024-05-21 17:02:48 -0600

    enti

---
+++ Entities/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Customer   //Adios puts
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string eMail { get; set; }
+
+        public int phoneNumber { get; set; }
+
+        List<Order_Confirmation> order_Confirmations { get; set; }
+    }
+}
